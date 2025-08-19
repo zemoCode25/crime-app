ebf1bd8b60ec233efc98644ad3a4edd827939145
Admin <ralphbryancarlos27@gmail.com>
2025-08-19 16:15:18 +0800

    feat: add bg to dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import DashboardPage from "./components/DashboardPage";
 
 export default function Page() {
-  return <div>Page</div>;
+  return <DashboardPage />;
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,13 +11,15 @@ export default function Page({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex justify-between pr-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex bg-neutral-100 justify-between pr-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1 dark:hover:bg-gray-800" />
           </div>
           <ThemeToggle />
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-neutral-100">
+          {children}
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
