963738ca8ca7fedd03067f32d25b3665a0cee31a
Admin <ralphbryancarlos27@gmail.com>
2025-08-19 21:52:21 +0800

    refactor: seperate card section

--- src/app/(main)/dashboard/components/DashboardPage.tsx
+++ src/app/(main)/dashboard/components/DashboardPage.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { Card } from "@/components/ui/card";
-import { TrendingDown } from "lucide-react";
-import { TrendingUp } from "lucide-react";
 import { useState } from "react";
+import CardSection from "./CardSection";
 import { ChevronDown } from "lucide-react";
 import * as React from "react";
 import { Check } from "lucide-react";
@@ -91,40 +89,7 @@ export default function DashboardPage() {
           </PopoverContent>
         </Popover>
       </div>
-      <div className="flex flex-col gap-4 sm:flex-row sm:gap-6">
-        <Card className="w-full h-fit !gap-0 p-4">
-          <h2>Total Crimes</h2>
-          <p className="text-4xl font-bold">1</p>
-          <div className="flex items-center gap-1">
-            <TrendingDown className="h-4 w-4 text-red-500" />
-            <span className="text-sm text-muted-foreground">-2 (2%)</span>
-          </div>
-        </Card>
-        <Card className="w-full h-30 !gap-0 p-4">
-          <h1>Crime Rate</h1>
-          <p className="text-4xl font-bold">5%</p>
-          <div className="flex items-center gap-1">
-            <TrendingUp className="h-4 w-4 text-gray-700" />
-            <span className="text-sm text-muted-foreground">22 (19%)</span>
-          </div>
-        </Card>
-        <Card className="w-full h-30 !gap-0 p-4">
-          <h1>Under Investigation</h1>
-          <p className="text-4xl font-bold">20</p>
-          <div className="flex items-center gap-1">
-            <TrendingDown className="h-4 w-4 text-red-500" />
-            <span className="text-sm text-muted-foreground">-2 (2%)</span>
-          </div>
-        </Card>
-        <Card className="w-full h-30 !gap-0 p-4">
-          <h1>Settled Case</h1>
-          <p className="text-4xl font-bold">40</p>
-          <div className="flex items-center gap-1">
-            <TrendingDown className="h-4 w-4 text-red-500" />
-            <span className="text-sm text-muted-foreground">-2 (2%)</span>
-          </div>
-        </Card>
-      </div>
+      <CardSection />
     </section>
   );
 }
