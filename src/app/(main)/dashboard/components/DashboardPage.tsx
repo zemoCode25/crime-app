2881c5d22ce96eff339d910ec28258ea2872ef0f
Admin <ralphbryancarlos27@gmail.com>
2025-08-19 16:44:16 +0800

    feat: add initial layout to dashboard cards

--- src/app/(main)/dashboard/components/DashboardPage.tsx
+++ src/app/(main)/dashboard/components/DashboardPage.tsx
@@ -1,5 +1,44 @@
-import React from "react";
-
+import { Card } from "@/components/ui/card";
+import { TrendingDown } from "lucide-react";
+import { TrendingUp } from "lucide-react";
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <section className="flex flex-col justify-between gap-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <div className="flex flex-col gap-4 sm:flex-row sm:gap-6">
+        <Card className="w-full h-30 !gap-0 p-4">
+          <h2>Total Crimes</h2>
+          <p className="text-4xl font-bold">100</p>
+          <div className="flex items-center gap-1">
+            <TrendingDown className="h-4 w-4 text-red-500" />
+            <span className="text-sm text-muted-foreground">-2 (2%)</span>
+          </div>
+        </Card>
+        <Card className="w-full h-30 !gap-0 p-4">
+          <h1>Crime Rate</h1>
+          <p className="text-4xl font-bold">5%</p>
+          <div className="flex items-center gap-1">
+            <TrendingUp className="h-4 w-4 text-gray-700" />
+            <span className="text-sm text-muted-foreground">22 (19%)</span>
+          </div>
+        </Card>
+        <Card className="w-full h-30 !gap-0 p-4">
+          <h1>Under Investigation</h1>
+          <p className="text-4xl font-bold">20</p>
+          <div className="flex items-center gap-1">
+            <TrendingDown className="h-4 w-4 text-red-500" />
+            <span className="text-sm text-muted-foreground">-2 (2%)</span>
+          </div>
+        </Card>
+        <Card className="w-full h-30 !gap-0 p-4">
+          <h1>Settled Case</h1>
+          <p className="text-4xl font-bold">40</p>
+          <div className="flex items-center gap-1">
+            <TrendingDown className="h-4 w-4 text-red-500" />
+            <span className="text-sm text-muted-foreground">-2 (2%)</span>
+          </div>
+        </Card>
+      </div>
+    </section>
+  );
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,7 +13,7 @@ export default function Page({ children }: { children: React.ReactNode }) {
       <SidebarInset>
         <header className="flex bg-neutral-100 justify-between pr-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1 dark:hover:bg-gray-800" />
+            <SidebarTrigger className="-ml-1 hover:bg-neutral-200 dark:hover:bg-gray-800" />
           </div>
           <ThemeToggle />
         </header>
