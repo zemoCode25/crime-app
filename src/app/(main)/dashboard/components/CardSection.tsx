345e84157cb425043beb3f4d4f680ef6cad0cf24
Admin <ralphbryancarlos27@gmail.com>
2025-08-23 23:17:22 +0800

    refactor: mobile responsiveness dashboard cards

--- src/app/(main)/dashboard/components/CardSection.tsx
+++ src/app/(main)/dashboard/components/CardSection.tsx
@@ -4,16 +4,16 @@ import { TrendingUp } from "lucide-react";
 
 export default function CardSection() {
   return (
-    <div className="flex flex-col gap-4 sm:flex-row sm:gap-6">
-      <Card className="w-full h-fit !gap-0 p-4">
-        <h2>Total Crimes</h2>
+    <div className="flex flex-row flex-wrap gap-4 sm:gap-6">
+      <Card className="w-[calc(50%-1rem)] md:w-[calc(25%-1.5rem)] h-fit !gap-0 p-4">
+        <h1>Total Crimes</h1>
         <p className="text-4xl font-bold">1</p>
         <div className="flex items-center gap-1">
           <TrendingDown className="h-4 w-4 text-red-500" />
           <span className="text-sm text-muted-foreground">-2 (2%)</span>
         </div>
       </Card>
-      <Card className="w-full h-30 !gap-0 p-4">
+      <Card className="w-[calc(50%-1rem)] md:w-[calc(25%-1.5rem)] h-30 !gap-0 p-4">
         <h1>Crime Rate</h1>
         <p className="text-4xl font-bold">5%</p>
         <div className="flex items-center gap-1">
@@ -21,15 +21,15 @@ export default function CardSection() {
           <span className="text-sm text-muted-foreground">22 (19%)</span>
         </div>
       </Card>
-      <Card className="w-full h-30 !gap-0 p-4">
-        <h1>Under Investigation</h1>
+      <Card className="w-[calc(50%-1rem)] md:w-[calc(27%-1.5rem)] h-full !gap-0 p-4">
+        <h1 className="text-sm sm:text-base">Under Investigation</h1>
         <p className="text-4xl font-bold">20</p>
         <div className="flex items-center gap-1">
           <TrendingDown className="h-4 w-4 text-red-500" />
           <span className="text-sm text-muted-foreground">-2 (2%)</span>
         </div>
       </Card>
-      <Card className="w-full h-30 !gap-0 p-4">
+      <Card className="w-[calc(50%-1rem)] md:w-[calc(23%-1.5rem)] h-full !gap-0 p-4">
         <h1>Settled Case</h1>
         <p className="text-4xl font-bold">40</p>
         <div className="flex items-center gap-1">
