3fda8f07b11e20ed073b77442a9c91269ad304c1
Admin <ralphbryancarlos27@gmail.com>
2025-08-27 09:35:46 +0800

    feat: add file crime form

---
+++ src/app/(main)/crime/cases/components/multi-step/CrimeForm.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// Report Date
+// Incident Date
+// Status
+// Type
+// Description
+// Description
+
+export default function CrimeForm() {
+  return (
+    <div>
+      <h1>Crime Form</h1>
+      <form action="">
+        <label htmlFor="">Crime Type</label>
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
