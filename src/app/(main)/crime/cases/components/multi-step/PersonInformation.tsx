04df5f59344bae08acbd53a054e744f10926f7cb
Admin <ralphbryancarlos27@gmail.com>
2025-08-27 09:53:13 +0800

    feat: add file person information form

---
+++ src/app/(main)/crime/cases/components/multi-step/PersonInformation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Combo box to select for suspect, complainant, witness
+// Button to add person information
+// ComboBox for role selection
+// Suspect (weapon_used, motive)
+// complainant (narrative, person-selection)
+// witness (testimony, person-selection)
+
+export default function PersonInformation() {
+  return (
+    <div>
+      <h1>Person Information</h1>
+    </div>
+  );
+}
