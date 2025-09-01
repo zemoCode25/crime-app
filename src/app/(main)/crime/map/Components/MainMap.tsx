cbb1382c9d28bbc8ddad19045359c5820092f282
Admin <ralphbryancarlos27@gmail.com>
2025-09-01 20:34:28 +0800

    feat: add red marker

--- src/app/(main)/crime/map/Components/MainMap.tsx
+++ src/app/(main)/crime/map/Components/MainMap.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { Button } from "@/components/ui/button";
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -12,10 +13,21 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 export default function AddressInformation() {
   return (
     <MapContainer
-      center={[14.451, 121.018]} // Alabang, Muntinlupa
+      center={[14.3798, 121.0249]} // Alabang, Muntinlupa
       zoom={13}
       scrollWheelZoom={true}
       className="z-0 min-h-80 w-full rounded-lg shadow-md"
@@ -24,6 +36,16 @@ export default function AddressInformation() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker position={[14.3794, 121.0249]} icon={redIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <Marker position={[14.3798, 121.0249]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
