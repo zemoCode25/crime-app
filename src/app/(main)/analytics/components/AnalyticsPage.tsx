fdc09d8427bdbc7b10a7cb7817c74a1a6afc8d34
Admin <ralphbryancarlos27@gmail.com>
2025-09-03 15:04:46 +0800

    feat: add crime bar

--- src/app/(main)/analytics/components/AnalyticsPage.tsx
+++ src/app/(main)/analytics/components/AnalyticsPage.tsx
@@ -1,11 +1,13 @@
 import CrimeChart from "./CrimeChart";
 import PieChartSection from "./PieChartSection";
+import { CrimeBar } from "./CrimeBar";
 export default function AnalyticsPage() {
   return (
     <div className="px-1 py-4">
       <h1 className="mb-4 text-2xl font-bold">Analytics</h1>
       <CrimeChart />
       <PieChartSection />
+      <CrimeBar />
     </div>
   );
 }
