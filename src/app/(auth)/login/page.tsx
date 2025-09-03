e4e1a87c7c8eeec541b72f54540c1912df26bbf7
Admin <ralphbryancarlos27@gmail.com>
2025-09-04 00:07:40 +0800

    refactor: quality

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
@@ -7,5 +7,5 @@ export default function LoginPage() {
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
