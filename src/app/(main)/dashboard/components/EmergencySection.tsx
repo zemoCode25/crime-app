e110c3c636c001f7e0fcf769a590195ae2997306
Admin <ralphbryancarlos27@gmail.com>
2025-09-04 12:38:04 +0800

    refactor: piechart content

--- src/app/(main)/dashboard/components/EmergencySection.tsx
+++ src/app/(main)/dashboard/components/EmergencySection.tsx
@@ -6,44 +6,44 @@ import Link from "next/link";
 export default function EmergencySection() {
   return (
     <section className="flex flex-col justify-between gap-4">
-      <h1 className="font-semibold text-lg">Give the community a heads up</h1>
-      <div className="flex flex-wrap flex-col sm:flex-row items-center justify-between !gap-1">
-        <Card className="px-6 py-4 flex-col w-full sm:w-[calc(33.333%_-_1rem)] !gap-0 flex">
+      <h1 className="text-lg font-semibold">Give the community a heads up</h1>
+      <div className="flex flex-col flex-wrap items-center justify-between !gap-1 sm:flex-row">
+        <Card className="flex w-full flex-col !gap-0 px-6 py-4 sm:w-[calc(33.333%_-_1rem)]">
           <div className="flex items-center gap-1">
             <Contact size={20} />
             <h1 className="text-lg font-bold">SMS</h1>
           </div>
-          <p className="text-sm w-full mb-2">Send emergency alerts via SMS</p>
+          <p className="mb-2 w-full text-sm">Send emergency alerts via SMS</p>
           <Link href="/dashboard/emergency/sms">
-            <Button className="!text-xs w-fit flex items-center gap-1 cursor-pointer">
+            <Button className="flex w-fit cursor-pointer items-center gap-1 !text-xs">
               <p>Send emergency</p>
               <Send size={12} />
             </Button>
           </Link>
         </Card>
-        <Card className="px-6 py-4 flex-col w-full sm:w-[calc(33.333%_-_1rem)] !gap-0 flex">
+        <Card className="flex w-full flex-col !gap-0 px-6 py-4 sm:w-[calc(33.333%_-_1rem)]">
           <div className="flex items-center gap-1">
-            <Contact size={20} />
+            <BellPlus size={20} />
             <h1 className="text-lg font-bold">Push Notification</h1>
           </div>
-          <p className="text-sm w-full mb-2">
+          <p className="mb-2 w-full text-sm">
             Send emergency alerts via Notification
           </p>
           <Link href="/dashboard/emergency/push">
-            <Button className="!text-xs w-fit flex items-center gap-1 cursor-pointer">
+            <Button className="flex w-fit cursor-pointer items-center gap-1 !text-xs">
               <p>Send emergency</p>
               <Send size={12} />
             </Button>
           </Link>
         </Card>
-        <Card className="px-6 py-4 flex-col w-full sm:w-[calc(33.333%_-_1rem)] !gap-0 flex">
+        <Card className="flex w-full flex-col !gap-0 px-6 py-4 sm:w-[calc(33.333%_-_1rem)]">
           <div className="flex items-center gap-1">
             <Mail size={20} />
             <h1 className="text-lg font-bold">Email</h1>
           </div>
-          <p className="text-sm w-full mb-2">Send emergency alerts via Email</p>
+          <p className="mb-2 w-full text-sm">Send emergency alerts via Email</p>
           <Link href="/dashboard/emergency/sms">
-            <Button className="!text-xs w-fit flex items-center gap-1 cursor-pointer">
+            <Button className="flex w-fit cursor-pointer items-center gap-1 !text-xs">
               <p>Send emergency</p>
               <Send size={12} />
             </Button>
---
+++ src/app/(main)/settings/page.tsx
@@ -0,0 +1,10 @@
+import SettingsPage from "./components/SettingsPage";
+
+export default function Page() {
+  return (
+    <div className="px-1 py-4">
+      <h1 className="mb-4 text-2xl font-bold">Settings</h1>
+      <SettingsPage />
+    </div>
+  );
+}
