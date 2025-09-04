0f0cb53d13634e7a9070a0a5b24b6474a00e9801
Admin <ralphbryancarlos27@gmail.com>
2025-09-04 23:39:40 +0800

    comment: todo in dashboard refactor

--- src/app/(main)/dashboard/components/DashboardPage.tsx
+++ src/app/(main)/dashboard/components/DashboardPage.tsx
@@ -43,6 +43,8 @@ export default function DashboardPage() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState<string>("Last 7 days");
 
+  // Seperate the sections with some spacing
+  // Seperate the popover component from the main dashboard component
   return (
     <section className="flex flex-col justify-between gap-4">
       <h1 className="mt-2 text-2xl font-bold">Dashboard</h1>
