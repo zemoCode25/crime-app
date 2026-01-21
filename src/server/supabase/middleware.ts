b42179de75668aa4c27efe664dffa07531e0530f
Admin <ralphbryancarlos27@gmail.com>
2026-01-21 08:51:48 +0800

    feat: notif

--- src/server/supabase/middleware.ts
+++ src/server/supabase/middleware.ts
@@ -2,6 +2,9 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/api/")) {
+    return NextResponse.next({ request });
+  }
   let supabaseResponse = NextResponse.next({
     request,
   });
